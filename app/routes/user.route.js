16a55a989584f6a8e3e60ad75e675ec93321fe2d
nohhongju <ghdwn49@gmail.com>
2022-04-11 18:23:31 +0900

    2022-04-11 saga

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -1,4 +1,4 @@
 const {signup, userlist} = require('../controllers/user.controller');
 module.exports = x => {x.app.post(`${x.url}/signup`, signup)
                        x.app.get(`${x.url}/list`, userlist)
-                       x.app.get(`${x.url}/list`, userlist)}
+                      }
