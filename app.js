d8ee5978063b27bf68422e864e973fceebeddb00
nohhongju <ghdwn49@gmail.com>
2022-04-12 10:08:17 +0900

    new branch

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const APP = './app/routes'
 // const nodes = ['admin','basic','board','game','todo','user']
 const nodes = ['board','user']
 for(const leaf of nodes){
-  require(`${APP}/${leaf}.route`)({url:`/api/${leaf}`,app})
+  
 }
 const corsOptions = {
   origin: 'http://localhost:3000',
